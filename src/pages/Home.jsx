2898862ed2005ff911c765e59411a8e06849e4ee
nikhilsinha822 <113183657+nikhilsinha822@users.noreply.github.com>
2023-08-09 04:52:14 +0530

    modify error and loader page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,40 @@
 import Feed from "../components/feed";
 import { useContext } from "react";
 import DataContext from "../context/DataContext";
+import loader from '../assets/loader.gif'
+import {BsPlugFill} from "react-icons/bs"
 import './home.css'
 
 const Home = () => {
   const {searchResults, fetchError, isLoading} = useContext(DataContext);
   return (
+    <>
+    {
+    isLoading ?
+    <div className="loader">
+        <img src={loader} alt="loader"/>
+        <h5>Hold on getting the latest post for you</h5>
+    </div>
+    :
+    fetchError ? 
+      <div className="error">
+          <BsPlugFill size="3rem"/>
+          {`Oops! there was a ${fetchError} :(`}
+          <br/>
+          Try refreshing the page or try again later.
+      </div>
+    :
     <main className="Home">
-      
-        {isLoading && <p>Loading the Post .......</p>}
-        {!isLoading && fetchError && <p style={{color:"red"}}>{fetchError}</p>}
-
-      { !isLoading && !fetchError &&
-          (searchResults.length ? (
+      {
+      (searchResults.length ? (
         <Feed posts={searchResults} />
       ) : (
         <p className="noPost">No posts to display</p>
-      ))}
-    </main>
+      ))
+      }
+    </main> 
+    }
+    </>
   );
 };
 
